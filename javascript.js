4e02fa51f5ff53f2bac40fbd6866c49ec04e803f
Carlos Garcia <cgarciapieto@gmail.com>
2018-08-13 10:57:24 -0500

    frontend-set-javascript-intro1-cgarciapieto

---
+++ javascript.js
@@ -0,0 +1,13 @@
+document.getElementById("changeThisHeader").innerText ="This is a better Header";
+
+// doucment.getElementById("newheader").innerText = "carlos";
+
+
+// var getsomething = document.getElementById("newheader");
+//
+// document.getElementById(getsomething + "carlos");
+
+
+
+
+document.getElementById("kennbutton").innerHTML = "carlos";
